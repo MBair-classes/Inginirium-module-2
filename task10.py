04207738a68cbad6e1ec5b3fc2dc5dc5494ded99
MBair-classes <126321377+MBair-classes@users.noreply.github.com>
2023-02-25 19:59:31 +0800

    Add files via upload
    Задание 10 тестирования 2 модуля

---
+++ task10.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+def greet_user():
+    username = name_field.get()
+    label.config(text=f"Привет, {username}.")
+
+root = Tk()
+root.geometry("200x200")
+label = Label(width = 30, height=2, bg = 'white', fg = 'black')
+button = Button(width = 15, height=5, bg = 'green', fg = 'black', text="Приветствие", command=greet_user)
+name_field = Entry(width = 15, bg = 'gray', fg = 'white')
+
+label.pack()
+button.pack()
+name_field.pack()
+
+root.mainloop()
